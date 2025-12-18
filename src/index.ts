df789086e82ae66eb14ca9ae55c7be1c76c78f5e
abg74123 <bisza_i.d@hotmail.com>
2025-12-18 15:10:50 +0700

    update interface

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * as AiFlowCrmBroadcast from './type-broadcast.js';
 export * as AiFlowCrmSubRole from './type-sub-role.js';
 export * as AiFlowCrmLog from './type-log.js';
 export * as AiFlowCrmDelivery from './delivery/index.js';
+export * as AiFlowCrmCurrency from './type-currency.js';
